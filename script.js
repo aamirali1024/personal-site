4e78215271303ea9f8f2680089db1a27ac42c084
aamirali1024 <aamir.ali1024@gmail.com>
2021-05-12 19:10:04 -0400

    JS started
    Added ending brace.
    
    JS is started and works exactly how I want it to.  Best implementation of animations is not event listeners, but messing with delay in CSS.  Set up observer to work on every section.

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
 $('body').scrollspy({ target: '#navbar-example' });
 
-document.addEventListener('scroll', function(e)) {
-    var y = Math.round(window.scrollY);
+// Element to observe
+var abt = document.querySelector('#aboutTab');
+    var abtHead = document.querySelector("#aboutHeading");
+    var abtBio = document.querySelector("#aboutBio");
+var edu = document.querySelector('#educationTab');
+var proj = document.querySelector('#projectTab');
+var skill = document.querySelector('#skillsTab');
+var cont = document.querySelector('#contactTab');
 
-    console.log(y);
-}
-function scroller() {
-    var y = Math.round(window.scrollY);
 
-    console.log(y)
-}
+//Create observer
+var observer = new IntersectionObserver(function (entries, obs) {
+	entries.forEach(function (entry) {
+		// Log if the element and if it's in the viewport
+		console.log(entry.target);
+		console.log(entry.isIntersecting);
+        
+        let isIntersecting = entry.isIntersecting 
+
+        // add animation class
+        if (isIntersecting == true) {
+            abt.classList.remove('visibile');
+            abt.classList.add('display');
+            abtHead.classList.add('fadeClass');
+            abtBio.classList.add('fadeClass');
+
+         // dispose observer
+        obs.unobserve(entry.target);
+        }  
+	});
+});
+
+// Attach to observer
+observer.observe(abt);
+// 809 1458 2110 2760 3254
