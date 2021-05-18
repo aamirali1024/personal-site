3b43870f7cf684132302584215e3e758605d6d53
aamirali1024 <aamir.ali1024@gmail.com>
2021-05-18 15:10:40 -0400

    Observers and Mailto done
    Mailto form is done. Observers are done and working. Setting up event listeners for animations.

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 $('body').scrollspy({ target: '#navbar-example' });
 
+// SVG
+var braceAni = document.querySelector('#braceAni');
+
 // Element to observe
 var abt = document.querySelector('#aboutTab');
-    var abtHead = document.querySelector("#aboutHeading");
-    var abtBio = document.querySelector("#aboutBio");
+    var abtHead = document.querySelector('#aboutHeading');
+    var abtBio = document.querySelector('#aboutBio');
 var edu = document.querySelector('#educationTab');
+    var eduHead = document.querySelector('#eduHeading');
 var proj = document.querySelector('#projectTab');
+    var projHead = document.querySelector("#projectHeading")
 var skill = document.querySelector('#skillsTab');
+    var skillsHead = document.querySelector('#skillsHead');
 var cont = document.querySelector('#contactTab');
+    var contHead = document.querySelector('#contHead');
 
 
 //Create observer
-var observer = new IntersectionObserver(function (entries, obs) {
-	entries.forEach(function (entry) {
-		// Log if the element and if it's in the viewport
+var observerA = new IntersectionObserver(function (entries, obs) {
+    entries.forEach(function (entry) {
+		// Log the element and if it's in the viewport
 		console.log(entry.target);
 		console.log(entry.isIntersecting);
         
@@ -21,17 +28,107 @@ var observer = new IntersectionObserver(function (entries, obs) {
 
         // add animation class
         if (isIntersecting == true) {
-            abt.classList.remove('visibile');
+            abt.classList.remove('visible');
             abt.classList.add('display');
             abtHead.classList.add('fadeClass');
-            abtBio.classList.add('fadeClass');
+            abtBio.classList.add('fadeClassB');
+            braceAni.addEventListener('animationend', function() {
+                braceAni.beginElement();
+            }, false);
 
          // dispose observer
-        obs.unobserve(entry.target);
+         obs.unobserve(entry.target);
         }  
 	});
 });
 
+//Create observer
+var observerB = new IntersectionObserver(function (entries, obs) {
+    entries.forEach(function (entry) {
+		// Log the element and if it's in the viewport
+		console.log(entry.target);
+		console.log(entry.isIntersecting);
+        
+        let isIntersecting = entry.isIntersecting 
+
+        // add animation class
+        if (isIntersecting == true) {
+            edu.classList.remove('visible');
+            edu.classList.add('display');
+            eduHead.classList.add('fadeClass');
+
+
+         // dispose observer
+         obs.unobserve(entry.target);
+        }  
+	});
+});
+
+var observerC = new IntersectionObserver(function (entries, obs) {
+    entries.forEach(function (entry) {
+		// Log the element and if it's in the viewport
+		console.log(entry.target);
+		console.log(entry.isIntersecting);
+        
+        let isIntersecting = entry.isIntersecting 
+
+        // add animation class
+        if (isIntersecting == true) {
+            proj.classList.remove('visible');
+            proj.classList.add('display');
+            projHead.classList.add('fadeClass');
+
+
+         // dispose observer
+         obs.unobserve(entry.target);
+        }  
+	});
+});
+
+var observerD = new IntersectionObserver(function (entries, obs) {
+    entries.forEach(function (entry) {
+		// Log the element and if it's in the viewport
+		console.log(entry.target);
+		console.log(entry.isIntersecting);
+        
+        let isIntersecting = entry.isIntersecting 
+
+        // add animation class
+        if (isIntersecting == true) {
+            skills.classList.remove('visible');
+            skills.classList.add('display');
+            skillsHead.classList.add('fadeClass');
+
+
+         // dispose observer
+         obs.unobserve(entry.target);
+        }  
+	});
+});
+
+var observerE = new IntersectionObserver(function (entries, obs) {
+    entries.forEach(function (entry) {
+		// Log the element and if it's in the viewport
+		console.log(entry.target);
+		console.log(entry.isIntersecting);
+        
+        let isIntersecting = entry.isIntersecting 
+
+        // add animation class
+        if (isIntersecting == true) {
+            cont.classList.remove('visible');
+            cont.classList.add('display');
+            contHead.classList.add('fadeClass');
+
+
+         // dispose observer
+         obs.unobserve(entry.target);
+        }  
+	});
+});
 // Attach to observer
-observer.observe(abt);
-// 809 1458 2110 2760 3254
+observerA.observe(abt);
+observerB.observe(edu);
+observerC.observe(proj);
+observerD.observe(skill);
+observerE.observe(cont);
