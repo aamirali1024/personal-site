ea6bf155ceb6bf922a218c1106c0a1981c8706ac
aamirali1024 <aamir.ali1024@gmail.com>
2021-05-31 16:05:46 -0400

    Structure and elements in place
    Everything is in place and functioning correctly. Next commit will be sizing for desktop, then media queries for mobile/tablet.

--- script.js
+++ script.js
@@ -4,34 +4,46 @@ $('body').scrollspy({ target: '#navbar-example' });
 var braceAni = document.querySelector('#braceAni');
 var gradient = document.querySelector('#test');
 var gradientB = document.querySelector('#test2');
+var gradientC = document.querySelector('#test3');
+var gradientD = document.querySelector('#test4');
+var gradientE = document.querySelector('#test5');
 
 // Element to observe
 var abt = document.querySelector('#aboutTab');
     var abtHead = document.querySelector('#aboutHeading');
     var abtBio = document.querySelector('#aboutBio');
     var abtPor = document.querySelector('#bio-pic');
+
 var edu = document.querySelector('#educationTab');
     var eduHead = document.querySelector('#eduHeading');
     var w3c = document.querySelector('#w3c');
     var rutgers = document.querySelector('#rutgers');
     var degree = document.querySelector('#degree');
     var cert = document.querySelector('#cert');
+
 var proj = document.querySelector('#projectTab');
-    var projHead = document.querySelector("#projectHeading");
-    var tabman = document.querySelector("#tabman");
+    var projHead = document.querySelector('#projectHeading');
+    var tabman = document.querySelector('#tabman');
     var tabmanDesc = document.querySelector('#tabman-desc');
     var pogflix = document.querySelector('#pogflix');
     var pogflixDesc = document.querySelector('#pogflix-desc');
+
 var skill = document.querySelector('#skillsTab');
     var skillsHead = document.querySelector('#skillsHead');
+    var html = document.querySelector('#html');
+    var css = document.querySelector('#css');
+    var js = document.querySelector('#js');
+    var bootstrap = document.querySelector('#bootstrap');
+
 var cont = document.querySelector('#contactTab');
     var contHead = document.querySelector('#contHead');
+    var form = document.querySelector('#myForm');
 
 const config = {
     threshold:0.5
 };
 
-
+// About Observer
 
 //Create observer
 var observerA = new IntersectionObserver(function (entries, obs) {
@@ -64,6 +76,8 @@ var observerA = new IntersectionObserver(function (entries, obs) {
         }  
 	});
 }, config);
+
+// Education Observer
 
 //Create observer
 var observerB = new IntersectionObserver(function (entries, obs) {
@@ -101,7 +115,9 @@ var observerB = new IntersectionObserver(function (entries, obs) {
          obs.unobserve(entry.target);
         }  
 	});
-});
+}, config);
+
+// Projects Observer
 
 var observerC = new IntersectionObserver(function (entries, obs) {
     entries.forEach(function (entry) {
@@ -115,13 +131,31 @@ var observerC = new IntersectionObserver(function (entries, obs) {
         if (isIntersecting == true) {
             proj.classList.remove('visible');
             projHead.classList.add('fadeClass');
+            // after heading animation
+            projHead.addEventListener('animationend', function(){
+                gradientC.beginElement();
+            }, false);
+            // after brace animation
+            gradientC.addEventListener('endEvent', function(){
+                tabman.classList.remove('visible');
+                tabmanDesc.classList.remove('visible');
+                pogflix.classList.remove('visible');
+                pogflixDesc.classList.remove('visible');
 
+                tabman.classList.add('fadeClass');
+                tabmanDesc.classList.add('fadeClass');
+                pogflix.classList.add('fadeClass');
+                pogflixDesc.classList.add('fadeClass');
+
+            },false);
 
          // dispose observer
          obs.unobserve(entry.target);
         }  
 	});
-});
+}, config);
+
+// Skills Observer
 
 var observerD = new IntersectionObserver(function (entries, obs) {
     entries.forEach(function (entry) {
@@ -135,13 +169,31 @@ var observerD = new IntersectionObserver(function (entries, obs) {
         if (isIntersecting == true) {
             skill.classList.remove('visible');
             skillsHead.classList.add('fadeClass');
+            // after heading animation
+            skillsHead.addEventListener('animationend', function(){
+                gradientD.beginElement();
+            }, false);
+            // after brace animation
+            gradientD.addEventListener('endEvent', function(){
+                html.classList.remove('visible');
+                css.classList.remove('visible');
+                js.classList.remove('visible');
+                bootstrap.classList.remove('visible');
 
+                html.classList.add('fadeClass');
+                css.classList.add('fadeClass');
+                js.classList.add('fadeClass');
+                bootstrap.classList.add('fadeClass');
+
+            },false);
 
          // dispose observer
          obs.unobserve(entry.target);
         }  
 	});
-});
+}, config);
+
+// Contact Observer
 
 var observerE = new IntersectionObserver(function (entries, obs) {
     entries.forEach(function (entry) {
@@ -155,13 +207,24 @@ var observerE = new IntersectionObserver(function (entries, obs) {
         if (isIntersecting == true) {
             cont.classList.remove('visible');
             contHead.classList.add('fadeClass');
+            // after heading animation
+            contHead.addEventListener('animationend', function(){
+                gradientE.beginElement();
+            }, false);
+            // after brace animation
+            gradientE.addEventListener('endEvent', function(){
+                form.classList.remove('visible');
 
+                form.classList.add('fadeClass');
+
+
+            },false);
 
          // dispose observer
          obs.unobserve(entry.target);
         }  
 	});
-});
+}, config);
 // Attach to observer
 observerA.observe(abt);
 observerB.observe(edu);
